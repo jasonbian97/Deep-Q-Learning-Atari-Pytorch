45eae91d77181bb7b5a1de5b82f5f9867d412e87
ZhangxingBian <jasonbian.zx@gmail.com>
2020-03-13 11:48:12 -0400

    Add dueling network

--- DQNs.py
+++ DQNs.py
@@ -161,25 +161,31 @@ class Dueling_DQN_2016_Modified(nn.Module):
     def __init__(self, num_classes=4, init_weights=True):
         super().__init__()
 
-        self.cnn = nn.Sequential(nn.Conv2d(4, 32, kernel_size=8, stride=4),
+        self.cnn = nn.Sequential(nn.Conv2d(4, 32, kernel_size=8, stride=4,bias=False),
                                         nn.ReLU(True),
-                                        nn.Conv2d(32, 64, kernel_size=4, stride=2),
+                                        nn.Conv2d(32, 64, kernel_size=4, stride=2,bias=False),
                                         nn.ReLU(True),
-                                        nn.Conv2d(64, 64, kernel_size=3, stride=1),
+                                        nn.Conv2d(64, 64, kernel_size=3, stride=1,bias=False),
+                                        nn.ReLU(True),
+                                        nn.Conv2d(64,1024,kernel_size=7,stride=1,bias=False),
                                         nn.ReLU(True)
                                         )
-        self.classifier = nn.Sequential(nn.Linear(7*7*64, 512),
-                                        nn.ReLU(True),
-                                        nn.Linear(512, num_classes)
-                                        )
+        self.streamA = nn.Linear(512, num_classes)
+        self.streamV = nn.Linear(512, 1)
+
         if init_weights:
             self._initialize_weights()
 
     def forward(self, x):
         x = self.cnn(x)
-        x = torch.flatten(x, start_dim=1)
-        x = self.classifier(x)
-        return x
+        sA,sV = torch.split(x,512,dim = 1)
+        sA = torch.flatten(sA,start_dim=1)
+        sV = torch.flatten(sV, start_dim=1)
+        sA = self.streamA(sA) #(B,4)
+        sV = self.streamV(sV) #(B,1)
+        # combine this 2 values together
+        Q_value = sV + (sA - torch.mean(sA,dim=1,keepdim=True))
+        return Q_value #(B,4)
 
     def _initialize_weights(self):
         for m in self.modules():
